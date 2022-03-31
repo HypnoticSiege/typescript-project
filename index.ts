8f129221050b401b38059f7fc935f4493e403559
Luis <hypnoticsiege@gmail.com>
2022-03-31 13:18:50 -0400

    :fish: DJS v13 + Minor Updates

--- index.ts
+++ index.ts
@@ -13,22 +13,25 @@ class Student {
     last: string;
     grade: number;
 
-    constructor(uid: string, dscid: number, first: string, last: string, grade: number) {
+    constructor(uid:string, dscid: number, first: string, last: string, grade: number) {
         this.uid = uid;
         this.dscid = dscid;
         this.first = first;
         this.last = last;
         this.grade = grade;
-    }
-}
+        db.push('/' + this.dscid, this);
+    };
+};
 
-const client = new Client()
+const client = new Client({
+    intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES']
+});
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user!.tag}. Ready to help ${client.users.cache.size} users in ${client.guilds.cache.size} servers!`);
-})
+});
 
-client.on("message", (message) => {
+client.on("messageCreate", (message) => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
 
@@ -44,51 +47,47 @@ client.on("message", (message) => {
                 .setDescription(`**Example:** \`\`${config.prefix}createprofile John Doe 10\`\``)
                 .setThumbnail(config.embed.logo)
                 .setColor(config.embed.color)
-                .setFooter(config.embed.footer)
-            message.channel.send(embed)
-        }
-        else {
+                .setFooter({ text: config.embed.footer })
+            message.channel.send({ embeds: [embed] });
+        } else {
             const student = new Student(uid, Number(message.author.id), first, last, Number(grade))
-            db.push('/' + message.author.id, student);
 
             const embed = new MessageEmbed()
                 .setTitle(`✅ Student Account Created!`)
                 .setDescription(`Congratulations, ${student.first} ${student.last}, your account was just created! Below is some key information you might need for your account.`)
                 .addField(`First Name:`, student.first)
                 .addField(`Last Name:`, student.last)
-                .addField(`Grade:`, student.grade)
+                .addField(`Grade:`, student.grade.toString())
                 .addField(`Unique ID:`, uid)
                 .setThumbnail(config.embed.logo)
                 .setColor(config.embed.color)
-                .setFooter(config.embed.footer)
-            message.channel.send(embed)
-        }
-
-    }
+                .setFooter({ text: config.embed.footer })
+            message.channel.send({ embeds: [embed] });
+        };
+    };
 
     if (command === 'profile') {
-        try {
-            var data = db.getData("/" + message.author.id)
+        const student = message.mentions.users.first() || message.author;
+        var data = db.getData("/" + student.id);
+        if (data) {
             const embed = new MessageEmbed()
                 .setTitle(data.first + " " + data.last)
-                .addField(`Grade:`, data.grade)
+                .addField(`Grade:`, data.grade.toString())
                 .addField(`Unique ID:`, data.uid)
                 .setThumbnail(config.embed.logo)
                 .setColor(config.embed.color)
-                .setFooter(config.embed.footer)
-            message.channel.send(embed)
-
-        } catch (err) {
+                .setFooter({ text: config.embed.footer })
+            message.channel.send({ embeds: [embed] });
+        } else {
             const embed = new MessageEmbed()
                 .setTitle(`❌ Could not find User Profile!`)
                 .setDescription(`We could not find a profile under your ID \`\`${message.author.id}\`\` \nTry making a profile using ${config.prefix}createprofile`)
                 .setThumbnail(config.embed.logo)
                 .setColor(config.embed.color)
-                .setFooter(config.embed.footer)
-            message.channel.send(embed)
-            console.error(err)
-        }
-    }
-})
+                .setFooter({ text: config.embed.footer })
+            message.channel.send({ embeds: [embed] });
+        };
+    };
+});
 
-client.login(config.token)
+client.login(config.token);
